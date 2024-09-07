6574c2bea175ff5097c6d176c902f55fb0de9b9f
phss-dev <pedro333h@gmail.com>
2024-09-06 23:08:51 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo: "Brown Lamination",
+        descricao: "O brown lamination é um tratamento capilar que tem ganhado popularidade por proporcionar um resultado natural e luminoso aos cabelos. A técnica consiste em aplicar uma coloração suave e transparente nos fios, intensificando a cor natural e promovendo um efeito de brilho intenso.",
+        link: "https://redeglobo.globo.com/rpc/especial-publicitario/bnyou/especial-e-ser-voce/noticia/sobrancelha-com-brow-lamination-saiba-tudo-sobre-a-tecnica-trend.ghtml",
+        tags: "preenchimento espetado"
+    },
+    {
+        titulo: "Lash Lifting",
+        descricao: "O lash lifting é um tratamento que eleva os cílios naturais, proporcionando um efeito de cílios mais longos e curvados, semelhante ao uso de um curvex. É uma ótima alternativa para quem busca um olhar mais aberto e expressivo.",
+        link: "https://www.bnyou.art/o-que-e-como-funciona-o-lash-lifting-e-brow/",
+        tags: "olhar auto-estima autoestima"
+    },
+     { 
+        titulo: "Design de Sobrancelhas",
+        descricao: "O design de sobrancelhas é um procedimento que visa modelar e organizar os pelos das sobrancelhas, realçando o olhar e harmonizando o rosto. A técnica pode envolver a depilação, a henna e a correção da forma.",
+        link: "https://www.stelamansur.com/curso-design-de-sobrancelhas/",
+        tags: "definição marcante desenho"
+        
+     }
+]
+
